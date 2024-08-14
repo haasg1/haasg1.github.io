7c6b52ec8a3ad3cd23759f488418d380e75f3b57
Gabriela Haas <47372136+haasg1@users.noreply.github.com>
2024-08-14 09:54:40 -0400

    Update media.js

--- src/app/consts/media.js
+++ src/app/consts/media.js
@@ -1,14 +1,8 @@
 import proxy from "../proxies/media"
 
 const media = {
-    discord: {
-        id: "",
-        tag: "",
-    },
-    
     linkedin: "gabrielachaas",
     github: "haasg1",
-    figma: "",
     email: "gabriela.haas48@gmail.com"
 
 }
